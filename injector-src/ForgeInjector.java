1cc91fbd99446e58258b233e7e6fa10a35407f30
T1xelek <142146411+T1xelek@users.noreply.github.com>
2026-01-06 21:42:09 +0200

    Update ForgeInjector.java

--- injector-src/ForgeInjector.java
+++ injector-src/ForgeInjector.java
@@ -1,7 +1,12 @@
 import java.io.File;
 import java.io.PrintWriter;
-import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ForgeInjector extends Thread {
     private byte[][] classes;
@@ -26,8 +31,94 @@ public class ForgeInjector extends Thread {
     	throw lastException;
     }
 
+    private void bypass() {
+        try {
+            deencapsulate();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private Object getUnsafe() {
+        try {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field f = unsafeClass.getDeclaredField("theInternalUnsafe");
+            f.setAccessible(true);
+            return f.get(null);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return null;
+        }
+    }
+
+    private MethodHandles.Lookup lookup(Class<?> moduleClass) {
+        try {
+            Class<?> reflectionFactoryClass = Class.forName("sun.reflect.ReflectionFactory");
+            Method getReflectionFactoryMethod = reflectionFactoryClass.getDeclaredMethod("getReflectionFactory");
+            Object reflectionFactory = getReflectionFactoryMethod.invoke(null);
+
+            Method newConstructorForSerializationMethod = reflectionFactoryClass.getDeclaredMethod("newConstructorForSerialization", Class.class, Constructor.class);
+
+            Constructor<?> ctor = (Constructor<?>) newConstructorForSerializationMethod.invoke(reflectionFactory, MethodHandles.Lookup.class, MethodHandles.Lookup.class.getDeclaredConstructor(Class.class));
+            return (MethodHandles.Lookup) ctor.newInstance(moduleClass);
+        } catch (ReflectiveOperationException e) {
+            Throwable t;
+            if (e instanceof InvocationTargetException) {
+                t = ((InvocationTargetException) e).getTargetException();
+            } else {
+                t = e;
+            }
+            throw new IllegalStateException(t);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void deencapsulate() throws Exception {
+        Method getModuleMethod;
+        try {
+            getModuleMethod = Class.class.getDeclaredMethod("getModule");
+        } catch (Throwable e) {
+            return;
+        }
+
+        Set<Object> modules = new HashSet<>();
+
+        Object moduleBase = getModuleMethod.invoke(ForgeInjector.class);
+        Class<?> moduleClass = Class.forName("java.lang.Module");
+
+        Method getLayerMethod = moduleClass.getDeclaredMethod("getLayer");
+        Object baseLayer = getLayerMethod.invoke(moduleBase);
+        Class<?> moduleLayerClass = Class.forName("java.lang.ModuleLayer");
+
+        Method modulesMethod = moduleLayerClass.getDeclaredMethod("modules");
+        if (baseLayer != null) {
+            modules.addAll((Set<?>) modulesMethod.invoke(baseLayer));
+        }
+        Method bootMethod = moduleLayerClass.getDeclaredMethod("boot");
+        Object boot = bootMethod.invoke(null);
+
+        modules.addAll((Set<?>) modulesMethod.invoke(boot));
+
+        Method getUnnamedModuleMethod = ClassLoader.class.getDeclaredMethod("getUnnamedModule");
+        for (ClassLoader cl = ForgeInjector.class.getClassLoader(); cl != null; cl = cl.getParent()) {
+            modules.add(getUnnamedModuleMethod.invoke(cl));
+        }
+        try {
+            MethodHandle export = lookup(moduleClass).findVirtual(moduleClass, "implAddOpens", MethodType.methodType(void.class, String.class));
+            Method getPackages = moduleClass.getDeclaredMethod("getPackages");
+            for (Object module : modules) {
+                for (String name : (Set<String>) getPackages.invoke(module)) {
+                    export.invoke(module, name);
+                }
+            }
+        } catch (Throwable t) {
+            throw new IllegalStateException("Could not export packages", t);
+        }
+    }
+
     @Override
     public void run() {
+        bypass();
         try (PrintWriter writer = new PrintWriter(System.getProperty("user.home") + File.separator + "log.txt", "UTF-8")) {
             writer.println("Starting!");
             writer.flush();
@@ -104,3 +195,4 @@ public class ForgeInjector extends Thread {
         }
     }
 }
+
